2df35e9dc9c6c52b3a645d10b21e35dfa0ebde1d
jon-jh <hiebert456@gmail.com>
2024-06-13 16:14:09 -0600

    wrote psuedocode and added if else functions

--- lunch.js
+++ lunch.js
@@ -1,39 +1,44 @@
-/*
- * Modify the contents of the function below, such that:
- *
- * If we're not hungry, we want to tell ourselves to get back to work.
- * Otherwise, we want to pick something up and eat it in the lab when
- * we've got less than 20 minutes or to try a place nearby if we've
- * got between 20 and 30 minutes. If we have any more time than that,
- * we want to remind ourselves that we're in a bootcamp and that we
- * should reconsider how much time we actually have to spare.
- *
- * hungry is a Boolean, representing if you're hungry or not.
- * availableTime is a Number representing the time you have for lunch,
- * in minutes.
- */
+/* Psuedocode:
+Are you hungry (Y/N) ? 
+If no, wait until you're hungry.
+If yes, what will you eat for lunch?
+That depends; how much time do you have?
+< 20 min, snack.
+20-30 min, cook a meal.
+> 30, reconsider.
+*/
 
-const whatToDoForLunch = function(hungry, availableTime) {
+const whatToDoForLunch = function (hungry, availableTime) {
   console.log("I don't know what to do!");
+  console.log("hungry is", hungry);
+  console.log("availableTime is", availableTime);
+
+  if (hungry === false) {
+    console.log("Wait until you are hungry!");
+  } else {
+    if (availableTime <= 20) {
+      console.log("The answer is have a snack.");
+    }
+    if (availableTime > 20 && availableTime < 31) {
+      console.log("The answer is cook a meal.");
+    }
+    if (availableTime > 30) {
+      console.log("Maybe reconsider how much time you have first.");
+    }
+  }
+
 }
-
-
-/*
- * This is some test runner code that's simply calling our whatToDoForLunch function
- * defined above to verify we're making the right decisions. Do not modify it!
- */
-
 console.log("I'm hungry and I have 20 minutes for lunch.");
 whatToDoForLunch(true, 20);
-console.log("---");
+console.log("-----");
 
 console.log("I'm hungry and I have 50 minutes for lunch.");
 whatToDoForLunch(true, 50);
-console.log("---");
+console.log("-----");
 
 console.log("I'm not hungry and I have 30 minutes for lunch.");
 whatToDoForLunch(false, 30);
-console.log("---");
+console.log("-----");
 
 console.log("I'm hungry and I have 15 minutes for lunch.");
 whatToDoForLunch(true, 15);
